ec857d6ca81486513161ca71c8ecc148189b3b4c
slow-wave <memoon_mj@icloud.com>
2023-02-08 18:31:31 +0900

    #4 middleware 추가

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,27 @@
 import express, { Express, Request, Response } from "express";
-
 import * as MySQLConnector from "./api/utils/mysql.connector";
+import cors from "cors";
+import session from "express-session";
+import passport from "passport";
+
 const app: Express = express();
 const port = 5000;
 
-// create database pool
+// create MySQL database pool
 MySQLConnector.init();
+
+//Middleware
+app.use(express.json());
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(
+  session({
+    secret: "code",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello world!");
