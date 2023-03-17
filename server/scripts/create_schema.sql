26b74b155be83e17e0bfcad84dc3906ed8b86ea5
slow-wave <memoon_mj@icloud.com>
2023-03-17 20:44:37 +0900

    record, tag 테이블 추가 #8

--- server/scripts/create_schema.sql
+++ server/scripts/create_schema.sql
@@ -3,6 +3,22 @@ CREATE DATABASE diary_system;
 CREATE TABLE `diary_system`.`user` (
 	`id` INT NOT NULL AUTO_INCREMENT,
     `username` VARCHAR(20) UNIQUE NOT NULL,
-    `password` VARCHAR(200) NOT NULL,
-    `isAdmin` TINYINT(1) NOT NULL,
+    `password` VARCHAR(20) NOT NULL,
+    `id_admin` TINYINT(1) NOT NULL,
     PRIMARY KEY (`id`));
+
+CREATE TABLE `diary_system`.`record` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `user_id` INT NOT NULL,
+    `datetime` DATETIME NOT NULL,
+    `content_title` TEXT(50) NOT NULL,
+    `content_main` LONGTEXT NOT NULL,
+    `content_image` LONGBLOB,
+    PRIMARY KEY(`id`));
+
+CREATE TABLE `diary_system`.`tag` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+	`record_id` INT NOT NULL,
+    `tag` TEXT(30) NOT NULL,
+    PRIMARY KEY(`id`)
+);
