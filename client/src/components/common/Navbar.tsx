ff59cbe089949b28aad7b48474b74b573611ff80
h12j21-star <jh06079@naver.com>
2023-04-12 18:25:29 +0900

    home navbar 아이콘 추가 #13

--- client/src/components/common/Navbar.tsx
+++ client/src/components/common/Navbar.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import logoImg from '../../assets/logo.png';
+import calendarImg from '../../assets/Category.png';
+import diaryImg from '../../assets/Chat.png';
+import settimtImg from '../../assets/Setting.png';
+import logoutImg from '../../assets/Logout.png';
 import {
     NavLayout,
     LogOutButton,
     NavList,
     LogoImg,
     NavLi,
+    IconImg,
 } from '../home/style/navbar';
 
 export default function Navbar() {
@@ -13,11 +18,23 @@ export default function Navbar() {
         <NavLayout>
             <LogoImg src={logoImg} alt="로고" />
             <NavList>
-                <NavLi>달력</NavLi>
-                <NavLi>일상 기록</NavLi>
-                <NavLi>마이 페이지</NavLi>
+                <NavLi>
+                    <IconImg src={calendarImg} alt="달력아이콘" />
+                    달력
+                </NavLi>
+                <NavLi>
+                    <IconImg src={diaryImg} alt="일상기록 아이콘" />
+                    일상 기록
+                </NavLi>
+                <NavLi>
+                    <IconImg src={settimtImg} alt="마이페이지 아이콘" />
+                    마이 페이지
+                </NavLi>
             </NavList>
-            <LogOutButton>Logout</LogOutButton>
+            <LogOutButton>
+                <IconImg src={logoutImg} alt="로그아웃버튼아이콘" />
+                Logout
+            </LogOutButton>
         </NavLayout>
     );
 }
