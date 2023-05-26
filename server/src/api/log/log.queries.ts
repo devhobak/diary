c0f9898c19ac7123dfac737c3b0b8c6604d09eb6
slow-wave <memoon_mj@icloud.com>
2023-05-27 00:08:04 +0900

    일상 추가 API 수정 #19

--- server/src/api/log/log.queries.ts
+++ server/src/api/log/log.queries.ts
@@ -1,7 +1,7 @@
 export const LogQueries = {
   AddLog: `
-      INSERT INTO diary_system.record (user_id, datetime, content_title, content_main, content_image) 
-      VALUES (?, now(), ?, ?, ?);`,
+      INSERT INTO diary_system.record (user_id, datetime, content_title, content_main) 
+      VALUES (?, now(), ?, ?);`,
   GetLogByDay: `
       SELECT * FROM diary_system.record WHERE user_id = ? and DATE(datetime) = ?;`,
   GetLogByMonth: `
--- server/src/api/log/log.service.ts
+++ server/src/api/log/log.service.ts
@@ -8,10 +8,8 @@ import { ILog } from "./log.model";
 export const insertLog = async (log: ILog) => {
   const result = await execute<{ affectedRows: number }>(LogQueries.AddLog, [
     log.user_id,
-    log.datetime,
     log.content_title,
     log.content_main,
-    log.content_image,
   ]);
   return result.affectedRows > 0;
 };
