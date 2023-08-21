e389d690ecaab0ebda3f998b8e42ab089f7e0384
h12j21-star <jh06079@naver.com>
2023-08-21 16:47:37 +0900

    회원 탈퇴 api 추가 #79

--- client/src/apis/api/Login.ts
+++ client/src/apis/api/Login.ts
@@ -1,5 +1,5 @@
-import { Api } from '../instance';
-import { AxiosResponse } from 'axios';
+import { Api, authApi } from '../instance';
+
 interface SignUpType {
     username?: FormDataEntryValue;
     password: FormDataEntryValue;
@@ -14,6 +14,7 @@ interface LoginType<R> {
     message: string;
     responseData: R;
 }
+
 const LoginCheck = async ({ ...data }: SignUpType, url: string) => {
     console.log({ ...data });
     try {
@@ -27,4 +28,15 @@ const LoginCheck = async ({ ...data }: SignUpType, url: string) => {
         return Promise.reject(error);
     }
 };
-export { LoginCheck };
+
+const Unregister = async (user_id: number, url: string) => {
+    try {
+        let res = await authApi.post(url, {
+            user_id: user_id,
+        });
+        return await res.data;
+    } catch (error) {
+        return Promise.reject(error);
+    }
+};
+export { LoginCheck, Unregister };
