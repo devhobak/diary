82c6f1f3fc2569e426d88974a4298af116a670ec
h12j21-star <jh06079@naver.com>
2023-09-01 14:44:23 +0900

    메뉴얼 스타일 및 기능 추가 #82

--- client/src/components/login/Login.tsx
+++ client/src/components/login/Login.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import { useState } from 'react';
 import Form from './Form';
 import { LoginArticle } from '../common/Form/style/box';
 import Logo from '../../assets/logo.png';
 import { useMediaQuery } from 'react-responsive';
+import Manual from '../manual/Manual';
 export default function Login(): JSX.Element {
     const isMobile = useMediaQuery({ maxWidth: 414 });
+    const [modal, setModal] = useState(true);
+
     return (
         <LoginArticle isMobile={isMobile}>
             <h2 className="ir">로그인 창</h2>
             <img src={Logo} alt="캘린더 로고" />
             <Form />
+            {modal ? <Manual modal={modal} setModal={setModal} /> : <></>}
         </LoginArticle>
     );
 }
---
+++ client/src/components/manual/style/manualstyle.ts
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+interface ViewType {
+    isMobile: boolean;
+}
+
+const ManualUl = styled.ul`
+    display: flex;
+    flex-direction: row;
+`;
+
+const ManualDiv = styled.div<ViewType>`
+    width: 100%;
+    height: 100%;
+    text-align: center;
+    background-color: #fffff3;
+    padding: ${(props) =>
+        props.isMobile ? '60px 20px' : '90px 50px 0px 50px'};
+    text-align: left;
+`;
+
+const ManualImg = styled.img`
+    width: 100%;
+    height: 100%;
+    margin-top: 16px;
+    margin-bottom: 16px;
+`;
+
+const ManualP = styled.p`
+    margin-bottom: 10px;
+    font-size: 3.2em;
+    font-weight: 800;
+    line-height: normal;
+`;
+
+const ManualSubP = styled.p`
+    color: #566271;
+    font-size: 2rem;
+    font-weight: 700;
+`;
+export { ManualUl, ManualDiv, ManualImg, ManualP, ManualSubP };
