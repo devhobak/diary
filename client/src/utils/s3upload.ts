929f3d02abb951c00b62a39701bf6d5f6fc64c68
h12j21-star <jh06079@naver.com>
2023-09-20 23:04:34 +0900

    프리티어 제거 " ->' 변경 #86

--- client/src/utils/s3upload.ts
+++ client/src/utils/s3upload.ts
@@ -1,40 +1,42 @@
-import AWS from "aws-sdk";
-import { toast } from "react-toastify";
+import AWS from 'aws-sdk';
+import { toast } from 'react-toastify';
 
 export default function s3upload(imageFile: File) {
-   const config = {
-      bucketName: process.env.REACT_APP_BUCKET_NAME,
-      region: process.env.REACT_APP_REGION,
-      accessKeyId: process.env.REACT_APP_ACCESS,
-      secretAccessKey: process.env.REACT_APP_SECRET,
-   };
+    const config = {
+        bucketName: process.env.REACT_APP_BUCKET_NAME,
+        region: process.env.REACT_APP_REGION,
+        accessKeyId: process.env.REACT_APP_ACCESS,
+        secretAccessKey: process.env.REACT_APP_SECRET,
+    };
 
-   AWS.config.update({
-      region: config.region,
-      accessKeyId: config.accessKeyId,
-      secretAccessKey: config.secretAccessKey,
-   });
+    AWS.config.update({
+        region: config.region,
+        accessKeyId: config.accessKeyId,
+        secretAccessKey: config.secretAccessKey,
+    });
 
-   const s3_params = {
-      ACL: "public-read",
-      Bucket: config.bucketName,
-      Key: `upload/${imageFile.name}`,
-      Body: imageFile,
-   };
+    const s3_params = {
+        ACL: 'public-read',
+        Bucket: config.bucketName,
+        Key: `upload/${imageFile.name}`,
+        Body: imageFile,
+    };
 
-   const uploadFile = (): AWS.S3.ManagedUpload.SendData | any => {
-      let clientS3 = new AWS.S3.ManagedUpload({ params: s3_params });
-      return clientS3
-         .promise()
-         .then(data => {
-            toast.success("이미지 업로드 완료!");
-            return data.Location;
-         })
-         .catch(err => {
-            toast.error("이미지 업로드에 오류가 발생했습니다. 관리자에게 문의부탁드립니다.");
-            return err;
-         });
-   };
+    const uploadFile = (): AWS.S3.ManagedUpload.SendData | any => {
+        let clientS3 = new AWS.S3.ManagedUpload({ params: s3_params });
+        return clientS3
+            .promise()
+            .then((data) => {
+                toast.success('이미지 업로드 완료!');
+                return data.Location;
+            })
+            .catch((err) => {
+                toast.error(
+                    '이미지 업로드에 오류가 발생했습니다. 관리자에게 문의부탁드립니다.'
+                );
+                return err;
+            });
+    };
 
-   return { uploadFile };
+    return { uploadFile };
 }
