144e7a7b7909dcfecff87281c7415691aeba4b61
h12j21-star <jh06079@naver.com>
2023-10-12 16:53:59 +0900

    hometest #93

--- client/src/test/Home.test.tsx
+++ client/src/test/Home.test.tsx
@@ -3,6 +3,9 @@ import Days from 'components/home/calendar/Days';
 
 import { render } from './provider-utils';
 import { fireEvent, screen } from '@testing-library/react';
+import Record from 'components/home/modal/Record';
+import InputSection from 'components/home/modal/InputSection';
+import Diary from 'components/home/modal/Diary';
 
 /**
  * @jest-environment jsdom
@@ -37,5 +40,22 @@ describe('Home 페이지 테스트', () => {
         fireEvent.click(todayLi);
         const modal = screen.getByText('일기 모달');
         expect(modal).toBeInTheDocument();
+    });
+
+    test('색상을 선택하면 모달 배경이 바뀐다.', () => {
+        const data = [
+            {
+                id: 1,
+                user_id: 1,
+                datetime: todaydate,
+                content_title: '일기제목',
+                content_main: '내용',
+                color: 'fff',
+                content_image: '',
+            },
+        ];
+        render(<Diary data={data} />);
+        const backgroud = screen.getByTestId('diary');
+        expect(backgroud).toHaveStyle({ color: data[0].color });
     });
 });
