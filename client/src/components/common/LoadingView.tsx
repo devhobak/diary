8367961b619a392e2d8a551ee357a88abe38c0e0
h12j21-star <jh06079@naver.com>
2023-10-22 16:16:30 +0900

    loading 분기로 처리 #84

--- client/src/components/common/LoadingView.tsx
+++ client/src/components/common/LoadingView.tsx
@@ -1,3 +1,23 @@
+import { useLocation } from 'react-router';
+import { useMediaQuery } from 'react-responsive';
+
+import ViewLayout from './Layout/ViewLayout';
+import { LoginArticle } from './Form/style/box';
+
+import { CalendarLayout } from '../home/style/style';
+
 export default function LoadingView({ children }: { children?: JSX.Element }) {
-    return <></>;
+    const { pathname } = useLocation();
+    const path = pathname.split('/')[1];
+    const isMobile = useMediaQuery({ maxWidth: 414 });
+
+    if (path === '' || path === 'signup') {
+        return <LoginArticle isMobile={isMobile}>{children}</LoginArticle>;
+    } else {
+        return (
+            <ViewLayout>
+                <CalendarLayout>{children}</CalendarLayout>
+            </ViewLayout>
+        );
+    }
 }
