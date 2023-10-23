d0e1d7573b2cb24ad5992e904d0f05c6db08e7d5
h12j21-star <jh06079@naver.com>
2023-10-23 16:46:31 +0900

    navli 객체 사용해서 출력 #41

--- client/src/components/common/Layout/Navbar.tsx
+++ client/src/components/common/Layout/Navbar.tsx
@@ -10,62 +10,65 @@ import { NavLayout, NavList, LogoImg, NavLi, IconImg } from './style/navbar';
 
 import { useLocation, useNavigate } from 'react-router';
 
+interface NavInfoType {
+    path: string;
+    img: string;
+    alt: string;
+    content: string;
+}
 export default function Navbar() {
     const navigate = useNavigate();
     let url = useLocation();
     let pathArr = ['/home', '/record', '/write', '/myPage'];
+    let navInfo: NavInfoType[] = [
+        {
+            path: '/home',
+            img: `${calendarImg}`,
+            alt: '달력아이콘',
+            content: '달력',
+        },
+        {
+            path: '/record',
+            img: `${diaryImg}`,
+            alt: '일상기록 아이콘',
+            content: '일상 기록',
+        },
+        {
+            path: '/write',
+            img: `${writeImg}`,
+            alt: '일기 작성 아이콘',
+            content: '일기 작성',
+        },
+        {
+            path: '/mypage',
+            img: `${settimtImg}`,
+            alt: '마이페이지 아이콘',
+            content: '마이 페이지',
+        },
+    ];
     let p = pathArr.indexOf(url.pathname) + 1;
     let [position, setPosition] = useState(p);
     const isMobile = useMediaQuery({ maxWidth: 980 });
     return (
         <NavLayout mobile={isMobile}>
             {isMobile ? <></> : <LogoImg src={logoImg} alt="로고" />}
-
             <NavList mobile={isMobile}>
-                <NavLi
-                    view={isMobile}
-                    onClick={() => {
-                        navigate('/home');
-                        setPosition(1);
-                    }}
-                    child={position}
-                >
-                    <IconImg src={calendarImg} alt="달력아이콘" />
-                    달력
-                </NavLi>
-                <NavLi
-                    view={isMobile}
-                    onClick={() => {
-                        navigate('/record');
-                        setPosition(2);
-                    }}
-                    child={position}
-                >
-                    <IconImg src={diaryImg} alt="일상기록 아이콘" />
-                    일상 기록
-                </NavLi>
-                <NavLi
-                    view={isMobile}
-                    onClick={() => {
-                        navigate('/write');
-                        setPosition(3);
-                    }}
-                    child={position}
-                >
-                    <IconImg src={writeImg} alt="일기 작성 아이콘" />
-                    일기 작성
-                </NavLi>
-                <NavLi
-                    view={isMobile}
-                    onClick={() => {
-                        navigate('/myPage');
-                        setPosition(4);
-                    }}
-                    child={position}
-                >
-                    <IconImg src={settimtImg} alt="마이페이지 아이콘" />
-                    마이 페이지
-                </NavLi>
+                {pathArr.map((path, index) => (
+                    <NavLi
+                        view={isMobile}
+                        onClick={() => {
+                            navigate(path);
+                            setPosition(index + 1);
+                        }}
+                        child={position}
+                    >
+                        <IconImg
+                            src={navInfo[index].img}
+                            alt={navInfo[index].alt}
+                        />
+                        {navInfo[index].content}
+                    </NavLi>
+                ))}
             </NavList>
         </NavLayout>
     );
